bd0b3fe5d9e6a33c925a06c122c9bf743ce8e514
CloveTseng <jessica.turn613@gmail.com>
2025-08-29 19:38:27 +0800

    add: index

--- src/components/HomepageNews/index.js
+++ src/components/HomepageNews/index.js
@@ -6,6 +6,7 @@ export default function HomepageNews() {
       <h2>最新消息</h2>
       <ul>
         <li>為加強前端技能的廣度，目前在了解 Vue 的觀念與實作，可以點擊<Link to="/docs/vue-daily/v-text">『Vue 每日學習筆記』</Link>查看</li>
+        <li>現正開發中：使用 `Nuxt.js` 及 `TailwindCSS` 協作開發 [OFFWORK](https://offwork.clovetseng.dev/)</li>
       </ul>
     </section>
   )
